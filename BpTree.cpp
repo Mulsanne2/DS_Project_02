8d41edf89ec6d42177d6e13018bdb15cd44e5b62
Mulsanne2 <kwonmincheol322@gmail.com>
2022-11-14 01:11:15 +0900

    Before split

--- BpTree.cpp
+++ BpTree.cpp
@@ -2,13 +2,74 @@
 
 bool BpTree::Insert(int key, set<string> set) {
 
+	BpTreeNode *FindingNode = searchDataNode(key); //find DataNode which has key value
+	/*
+	check if node hasn't found
+	*/
+	if(FindingNode==NULL){ //if node don't have key value then make new Frequent Pattern Node
+
+		FrequentPatternNode *NewFreNode = new FrequentPatternNode;
+		NewFreNode->setFrequency(key); // insert key in Freqeunt patterNode
+		NewFreNode->InsertList(set);
+		
+		if(root==NULL){ //when make the fisrt node and set to root
+			BpTreeNode *NewDataNode = new BpTreeDataNode;
+			NewDataNode->insertDataMap(key, NewFreNode);
+			root = NewDataNode; //set root
+		}
+		else if(!root->getMostLeftChild()){ //check while insert in root
+			root->insertDataMap(key, NewFreNode);
+			if(excessDataNode(root)) //check root has been over
+				splitDataNode(root);
+		}
+		else{
+		BpTreeNode* CurNode = root;
+		while(CurNode->getMostLeftChild()) //move to last node
+			CurNode = CurNode->getMostLeftChild();
+
+		while(CurNode->getNext()){ //move to right arrange
+			if(key<CurNode->getNext()->getDataMap()->begin()->first)
+				break;
+			CurNode = CurNode->getNext();
+		}
+		CurNode->insertDataMap(key, NewFreNode);
+		if(excessDataNode(CurNode))
+			splitDataNode(CurNode);
+
+		CurNode = CurNode->getParent();
+		while(CurNode){
+			if(excessIndexNode(CurNode))
+				splitIndexNode(CurNode);
+			CurNode = CurNode->getParent();
+		}
+		if(root->getParent())
+			root = root->getParent();
+		}
+	}
+	else{
+		FindingNode->getDataMap()->find(key)->second->InsertList(set); //put item in insert list
+	}
+
 	return true;
 }
 
 BpTreeNode* BpTree::searchDataNode(int n) {
 	BpTreeNode* pCur = root;
 	
-	return pCur;
+	if(root==NULL) //check if root is NULL
+		return NULL;
+
+	while( pCur->getMostLeftChild()){ //Move to the Left child from the bottom 
+		pCur = pCur->getMostLeftChild();
+	}
+	
+	while(pCur){ //get the 
+		map<int, FrequentPatternNode *> *CurMap = pCur->getDataMap(); 
+		if(CurMap->find(n)!=CurMap->end())
+			return pCur;
+		pCur = pCur->getNext();
+	}
+	return NULL;
 }
 
 void BpTree::splitDataNode(BpTreeNode* pDataNode) {
